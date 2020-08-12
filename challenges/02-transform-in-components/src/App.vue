66171d89bc0577a29547d29ec3311533cd9989fe
caiqueubasp <caiqueubasp@gmail.com>
2020-08-11 22:32:57 -0300

    adding first project using components and  the exercise 02-transforme-in-components with only 1 component concluided

---
+++ challenges/02-transform-in-components/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <div>
+      <app-server-status></app-server-status>
+    </div>
+    <hr />
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+          <li class="list-group-item" v-for="index in 5">
+            Server #{{ index }}
+          </li>
+        </ul>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <p>Server Details are currently not updated</p>
+      </div>
+    </div>
+    <hr />
+    <div class="row">
+      <div class="col-xs-12">
+        <footer>
+          <p>All Servers are managed here</p>
+        </footer>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ServerStatus from "./ServerStatus.vue";
+export default {
+  components: {
+    "app-server-status": ServerStatus
+  }
+};
+</script>
+
+<style></style>
---
+++ project-1/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <app-server-status></app-server-status>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      status: "Critical"
+    };
+  }
+};
+</script>
+
+<style></style>
