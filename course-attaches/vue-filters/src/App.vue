287af6ad91f59ebae0728478be8f5a58344066c5
caiqueubasp <caiqueubasp@gmail.com>
2020-08-24 21:14:46 -0300

    starting with filters"

---
+++ course-attaches/vue-filters/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Filters & Mixins</h1>
+        <p>{{ text | myToUppercase | to-lowercase}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      text: "Hello There!",
+    };
+  },
+  filters: {
+    myToUppercase(value) {
+      return value.toUpperCase();
+    },
+  },
+};
+</script>
+
+<style>
+</style>
