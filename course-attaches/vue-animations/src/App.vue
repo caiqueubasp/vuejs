c941ee0a59e13e9679aa3a622647201e189582a7
caiqueubasp <caiqueubasp@gmail.com>
2020-08-29 18:18:00 -0300

    finish vuejs animation modules and the vuejs quiz project with animation

--- course-attaches/vue-animations/src/App.vue
+++ course-attaches/vue-animations/src/App.vue
@@ -38,23 +38,134 @@
           <div class="alert alert-info" v-if="show" key="info">This is some info HHHH</div>
           <div class="alert alert-warning" v-else key="warning">This is some warning</div>
         </transition>
+        <hr />
+        <button class="btn btn-primary" @click="load = !load">Load / Remove Element</button>
+        <br />
+        <br />
+        <transition
+          @before-enter="beforeEnter"
+          @enter="enter"
+          @after-enter="afterEnter"
+          @enter-cancelled="enterCancelled"
+          @before-leave="beforeLeave"
+          @leave="leave"
+          @after-leave="afterLeave"
+          @leave-cancelled="leaveCancelled"
+          :css="false"
+        >
+          <div style="width: 300px; height: 100px; background-color: lightgreen" v-if="load"></div>
+        </transition>
+        <hr />
+        <button
+          class="btn btn-primary"
+          @click="selectedComponent == 'app-success-alert' ? selectedComponent = 'app-danger-alert' : selectedComponent = 'app-success-alert'"
+        >Toggle Components</button>
+        <br />
+        <transition name="fade" mode="out-in">
+          <component :is="selectedComponent"></component>
+        </transition>
+        <hr />
+        <button class="btn btn-primary" @click="addItem">Add Item</button>
+        <br />
+        <br />
+        <ul class="list-group">
+          <transition-group name="slide">
+            <li
+              class="list-group-item"
+              v-for="(number, index) in numbers"
+              :key="number"
+              @click="removeItem(index)"
+              style="cursor: pointer"
+            >{{ number }}</li>
+          </transition-group>
+        </ul>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import dangerAlert from "./components/DangerAlert";
+import successAlert from "./components/SuccessAlert";
+
 export default {
   data() {
     return {
       show: false,
+      load: true,
       alertAnimation: "",
+      elementWidth: 100,
+      selectedComponent: "app-success-alert",
+      numbers: [1, 2, 3, 4, 5],
     };
+  },
+  methods: {
+    beforeEnter(el) {
+      console.log("beforeEnter");
+      this.elementWidth = 100;
+      el.style.width = this.elementWidth + "px";
+    },
+    enter(el, done) {
+      console.log("enter");
+      let round = 1;
+      const interval = setInterval(() => {
+        el.style.width = this.elementWidth + round * 10 + "px";
+        round++;
+        if (round > 20) {
+          clearInterval(interval);
+        }
+      }, 20);
+      done();
+    },
+    afterEnter(el) {
+      console.log("afterEnter");
+    },
+    enterCancelled(el) {
+      console.log("beforeLeave");
+    },
+    beforeLeave(el) {
+      console.log("beforeLeave");
+      this.elementWidth = 300;
+      el.style.width = this.elementWidth + "px";
+    },
+    leave(el, done) {
+      console.log("leave");
+      let round = 1;
+      const interval = setInterval(() => {
+        el.style.width = this.elementWidth + round * 10 + "px";
+        round++;
+        if (round > 20) {
+          clearInterval(interval);
+        }
+      }, 20);
+      done();
+    },
+    afterLeave(el) {
+      console.log("afterLeave");
+    },
+    leaveCancelled(el) {
+      console.log("leaveCancelled");
+    },
+    addItem() {
+      const position = Math.floor(Math.random() * this.numbers.length);
+      this.numbers.splice(position, 0, this.numbers.length + 1);
+    },
+    removeItem(index) {
+      this.numbers.splice(index, 1);
+    },
+  },
+  components: {
+    appDangerAlert: dangerAlert,
+    appSuccessAlert: successAlert,
   },
 };
 </script>
 
 <style>
+.container {
+  margin-bottom: 200px;
+}
+
 .fade-enter {
   opacity: 0;
 }
@@ -89,6 +200,11 @@ export default {
   animation: slide-out 1s ease-out forwards;
   transition: opacity 1s;
   opacity: 0;
+  position: absolute;
+}
+
+.slide-move {
+  transition: all 1s;
 }
 
 @keyframes slide-in {
